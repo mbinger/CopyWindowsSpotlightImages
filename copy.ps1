f18ff1e1051619b07744cecd12a401a1ee9f7f16
mbinger <45658386+mbinger@users.noreply.github.com>
2019-08-23 07:37:20 +0200

    Add try-catch

--- copy.ps1
+++ copy.ps1
@@ -18,13 +18,17 @@ Foreach-Object {
 	$destinationBadFileName = (Join-Path $currentPath "bad" | Join-Path -childpath $fileName) + ".jpg"
 
 	if (![System.IO.File]::Exists($destinationFileName) -and ![System.IO.File]::Exists($destinationBadFileName)) {
-		$img = [System.Drawing.Image]::FromFile($sourceFileName)
-		$width = $img.Width
-		$height = $img.Height
-		$img.Dispose()    
-		
-		if (($width -gt 1000) -and ($width -gt $height)) {
-			Copy-Item $sourceFileName -Destination $destinationFileName
+		try {
+			$img = [System.Drawing.Image]::FromFile($sourceFileName)
+			$width = $img.Width
+			$height = $img.Height
+			$img.Dispose()    
+			
+			if (($width -gt 1000) -and ($width -gt $height)) {
+				Copy-Item $sourceFileName -Destination $destinationFileName
+			}
+		} catch [system.exception] {
+			"caught a system exception: " +  $sourceFileName
 		}
 	}
 }
